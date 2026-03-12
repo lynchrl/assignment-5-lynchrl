37200803b0ef5897ba47708f07ff92770aff31e0
Ryan Lynch <lynchrl@colorado.edu>
2026-03-12 09:32:52 -0600

    Update assignment 3 commit ref

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '8f9ecdb76aa39851624e9f31816e5b83973bde86'
+AESD_ASSIGNMENTS_VERSION = 'decc869a463ae97d5535318a55ffdbf54a39ef8b'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
