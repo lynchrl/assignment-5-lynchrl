f045db58e00d070012beff210b41eb645878c966
Ryan Lynch <lynchrl@colorado.edu>
2026-03-26 19:35:43 -0600

    Installation of scripts, initial overlay, ref fixed scripts repo hash, set pw

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,4 +1,4 @@
-LDD_VERSION = '89845b73a31266da90e4ee7e0cfa57fd55bc591f'
+LDD_VERSION = '11def765cff98dffee36e505a7d45b57277ce7ab'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -6,19 +6,15 @@ LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-lynchrl.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-# LDD_MODULE_MAKE_OPTS += ccflags-y="-I$(@D)/include" \
-# 	ccflags-y="-I$(@D)/misc-modules/include" \
-# 	ccflags-y="-I$(@D)/scull/include"
-# LDD_MODULE_MAKE_OPTS = \
-#     ARCH=$(KERNEL_ARCH) \
-#     KERNELDIR=$(LINUX_DIR) \
-#     KVERSION=$(LINUX_VERSION_PROBED) \
-#     ccflags-y="-I$(@D)/include" \
-# 	ccflags-y="-I$(@D)/misc-modules/include" \
-# 	ccflags-y="-I$(@D)/scull/include"
-
 LDD_MODULE_SUBDIRS = misc-modules
 LDD_MODULE_SUBDIRS += scull
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/sbin
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
